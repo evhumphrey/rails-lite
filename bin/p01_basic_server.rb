0a0af23c9d4c32fcd8ce57d59cc701a044c3af69
Elliot Humphrey <evhumphrey@users.noreply.github.com>
2017-04-25 13:57:32 -0700

    Implement simple rack app

--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -1,1 +1,15 @@
 require 'rack'
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+
+  res['Content-type'] = 'text/html'
+  res.write('Hello World')
+  res.finish
+end
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
